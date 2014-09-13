9a46711cd6378a15e150d64ff2d0ddadecb617bc
romangeiko <romangeiko@mail.ru>
2014-09-13 15:43:54 +0400

    com 2

--- example.f90
+++ example.f90
@@ -1,3 +1,16 @@
-program example
-print *, "I need your clothes!"
-end program
+      program plotter
+      open (11, FILE='BOT')
+      pi = 4.*atan(1.)
+      a=pi/100
+      do 2, j = 1, 1000
+      t=a*j
+      sum = 0.
+      h=t/1000
+      do 1, i = 1 , 1000
+      z=h*i
+      sum = h * sin (z) + sum
+ 1    continue
+      write (11, *) t, sum
+ 2    continue
+      close (11)
+      end
